b8fc2635bc6bd88d1b94d742580d8ffe42bfe146
Matt Weiss <weiss.matt@outlook.com>
2019-01-30 09:37:34 -0700

    Add Iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -11,4 +11,13 @@ class House
     @rooms << room
   end
 
+  def rooms_from_category(category)
+    @rooms.select {|room| room.category == category}
+  end
+
+  def area
+    @rooms.inject(0){|sum, room| sum + room.area}
+  end
+
+  
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -8,6 +8,8 @@ class HouseTest < Minitest::Test
     @house = House.new("$400000", "123 sugar lane")
     @room_1 = Room.new(:bedroom, 10, 13)
     @room_2 = Room.new(:bedroom, 11, 15)
+    @room_3 = Room.new(:living_room, 25, 15)
+    @room_4 = Room.new(:basement, 30, 41)
   end
 
   def test_it_exists
@@ -32,5 +34,23 @@ class HouseTest < Minitest::Test
 
     assert_equal 2, @house.rooms.length
   end
-  
+
+  def test_rooms_can_sort_by_category
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+
+    assert_equal 2, @house.rooms_from_category(:bedroom).length
+    assert_equal 1, @house.rooms_from_category(:basement).length
+  end
+
+  def test_house_area
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+
+    assert_equal 1900, @house.area
+  end
 end
